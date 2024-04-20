82b44e29d55d2536cf3e4ea52ec78c6d7bcbb942
Mohammed A <mouhammedmax@hotmail.com>
2024-04-20 16:12:01 +0100

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:C22375093@mytudublin.ie">C22375093@mytudublin.ie</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>8am to 10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">TUD, Grangegorman Lower, Co. Dublin</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+353-456-7890">+353-456-7890</a>
+         <a href="tel:+353-222-3333">+353-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span><a href="mailto:C22375093@mytudublin.ie">Mohammed Abourass, C22375093</a></span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
